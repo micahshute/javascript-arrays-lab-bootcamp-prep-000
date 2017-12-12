c6444cb05b80198f1e99d675319212cc169cfe1b
Micah Shute <micah.shute@gmail.com>
2017-12-12 23:25:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ function destructivelyAppendKitten(name){
   return window.kittens
 }
 
+function destructivelyPrependKitten(name){
+  window.kittens = restoreKittens()
+  window.kittens.unshift(name)
+  return window.kittens
+}
+
 function destructivelyRemoveLastKitten(){
   window.kittens = restoreKittens()
   window.kittens.pop()
@@ -25,5 +31,21 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  
+  window.kittens = restoreKittens()
+  return [...window.kittens, name]
+}
+
+function prependKitten(name){
+  window.kittens = restoreKittens()
+  return [name, ...window.kittens]
+}
+
+function removeLastKitten(){
+  window.kittens = restoreKittens()
+  return window.kittens.slice(0,window.kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  window.kittens = restoreKittens()
+  return window.kittens.slice(1)
 }
